31994341fb0998356009a928a835c6d9eb2ea0ad
aslbekadizov2001 <aslbekadizov2001@gmail>
2022-12-17 12:56:52 +0500

    first

---
+++ script.js
@@ -0,0 +1,93 @@
+const musicContainer = document.getElementById('audio-container')
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+
+const audio = document.getElementById('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
+
+
+const songs = ['Rema - Calm Down', 'cukur', 'Marshmello-Khalid-Numb-320', "I'm a criminal", 'Aykos']
+
+
+let songIndex = 0
+
+function loadSong(song){
+    title.innerText = song
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
+}
+
+loadSong(songs[songIndex])
+
+function playSong(){
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+
+function pauseSong(){
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+
+function prevSong(){
+    songIndex--;
+    if(songIndex<0){
+        songIndex = songs.length-1
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+
+function nextSong(){
+    songIndex++;
+
+    if(songIndex> songs.length-1){
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+function updateProgress(e){
+    const {duration, currentTime} =e.srcElement
+    const updateProgress = (currentTime / duration) * 100
+    progress.style.width = `${updateProgress}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+ }
+
+ playBtn.addEventListener('click', function() {
+    const isPlaying = musicContainer.classList.contains('play')
+    if(isPlaying){
+        pauseSong()
+    }
+    else{
+        playSong()
+    }
+ })
+
+ prevBtn.addEventListener('click', prevSong)
+ nextBtn.addEventListener('click', nextSong)
+ audio.addEventListener('timeupdate', updateProgress)
+ progressContainer.addEventListener('click', setProgress)
+ audio.addEventListener('ended', nextSong)
